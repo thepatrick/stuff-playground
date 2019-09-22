0322239eccf7752ab7f8d1017e40cc470075da82
Patrick Quinn-Graham <make-contact@pftqg.com>
2019-09-23 09:14:38 +1000

    Initial Commit

---
+++ StuffPlaygroundTests/StuffPlaygroundTests.swift
@@ -0,0 +1,34 @@
+//
+//  StuffPlaygroundTests.swift
+//  StuffPlaygroundTests
+//
+//  Created by Patrick Quinn-Graham on 23/9/19.
+//  Copyright © 2019 Patrick Quinn-Graham. All rights reserved.
+//
+
+import XCTest
+@testable import StuffPlayground
+
+class StuffPlaygroundTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
